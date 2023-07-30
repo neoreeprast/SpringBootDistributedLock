400d7ed3caca4eab8e5a601ccad0bc2acd371f0e
Arie <arie@rumanira.com>
2023-07-30 22:43:45 +0700

    Initial Commit

---
+++ src/main/java/com/rumanira/springbootdistributedlock/SpringBootDistributedLockApplication.java
@@ -0,0 +1,13 @@
+package com.rumanira.springbootdistributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDistributedLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDistributedLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rumanira/springbootdistributedlock/SpringBootDistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumanira.springbootdistributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
